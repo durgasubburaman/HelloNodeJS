352b4daf805a7d7e950ac0779c170bb61828c375
Julien Corioland <julien.corioland@live.fr>
2016-11-15 09:07:36 +0100

    update to v1.1

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var os = require('os');
 var port = process.env.PORT || 5001;   
 
 app.get('/', function(req, res) {
-    res.json('Hello nodejs from Azure Web App on Linux - Version 1.0 - Host name : ' + os.hostname());
+    res.json('Hello nodejs from Azure Web App on Linux - Version 1.1 - Host name : ' + os.hostname());
 });
 
 // start the app
