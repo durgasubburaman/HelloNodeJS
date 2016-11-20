5cb16066bff246fbfc04632d4624a3c5942f7cd0
Julien Corioland <julien.corioland@live.fr>
2016-11-20 19:39:02 +0100

    v1.0

--- server.js
+++ server.js
@@ -2,15 +2,12 @@
 
 var express = require('express');
 var app = express();
-var os = require('os');
 
 var port = process.env.PORT || 5001;   
 
 app.get('/', function(req, res) {
-    res.json('Hello nodejs from Azure Web App on Linux - Version 1.1 - Host name : ' + os.hostname());
+    res.json('Hello nodejs from Azure Web App on Linux - Version 1.0');
 });
 
 // start the app
 app.listen(port);
-
-console.log("Application listening on http://localhost:"+port);
